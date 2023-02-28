7708e11ac42da85bbfed34b9ba4af28fb068a82c
Leon Work <26638409+Ddraigan@users.noreply.github.com>
2023-02-28 16:23:35 +0000

    mime dont work..

--- src-tauri/src/commands/get_apps.rs
+++ src-tauri/src/commands/get_apps.rs
@@ -1,18 +1,33 @@
 use crate::services::file_handler::get_dir_items;
-use infer::get;
 use tauri::command;
+//r#"C:\ProgramData\Microsoft\Windows\Start Menu\Programs"#
 
 #[command]
 pub fn get_apps() -> Vec<String> {
-    let files = get_dir_items(r#"C:\ProgramData\Microsoft\Windows\Start Menu\Programs"#);
+    let files = get_dir_items(r#"C:\Users\Web.RNW\Pictures"#);
     // let mut files_new = vec![];
     // for file in files {
     //     let file_name = file.display().to_string();
     //     files_new.push(file_name)
     // }
     // files_new
-    let mut new_files = vec![];
+    let mut new_files: Vec<String> = vec![];
     for file in files {
-        infer::get_from_path(file);
-    };
+        let kind_opt = match infer::get_from_path(&file) {
+            Ok(kind_opt) => kind_opt,
+            Err(_) => continue,
+        };
+
+        match kind_opt {
+            Some(kind) => {
+                if kind.mime_type() == "image/jpg" {
+                    println!("{:?}", file);
+                    new_files.push(file.display().to_string());
+                }
+            }
+            None => continue,
+        }
+    }
+
+    new_files
 }
