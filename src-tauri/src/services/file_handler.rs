bb2f8e62680984f2092f2bd0277d827caef6389b
Ddraigan <26638409+Ddraigan@users.noreply.github.com>
2023-03-01 20:22:46 +0000

    Refactored mime type handler into own file

--- src-tauri/src/services/file_handler.rs
+++ src-tauri/src/services/file_handler.rs
@@ -1,31 +1,7 @@
-use infer::Type;
+use crate::services::mime_type_handler::{get_custom_infer, get_matcher_type};
 use std::{fs::read_dir, path::PathBuf};
 
-const LNK_MIME_TYPE: &str = "application/x-ms-shortcut";
-
-// 4c 00 00 00 01 14 02 00 00 00 00 00 c0 00 00 00 L...............
-// 00 00 00 46 8b 00 08 00 11 00 00 00 9f f6 c0 fd ...F............
-// bd cb cc 01 42 94 c9 85 92 cc cc 01 5d 6c d7 d2 ....B........l..
-// c8 cc cc 01 00 00 00 00 00 00 00 00 01 00 00 00 ................
-
-fn custom_matcher(buf: &[u8]) -> bool {
-    return buf.len() >= 3 && buf[0] == 0x4c && buf[1] == 0x00 && buf[2] == 0x00;
-}
-
-fn get_custom_infer() -> infer::Infer {
-    let mut info = infer::Infer::new();
-    info.add(LNK_MIME_TYPE, "lnk", custom_matcher);
-    info
-}
-
-fn get_matcher_type(mime_type: Type) -> infer::MatcherType {
-    match mime_type.mime_type() {
-        LNK_MIME_TYPE => infer::MatcherType::App,
-        _ => mime_type.matcher_type(),
-    }
-}
-
-pub fn get_dir_items<'a>(path: &str) -> Vec<PathBuf> {
+pub fn get_dir_items(path: &str) -> Vec<PathBuf> {
     let mut files_array: Vec<PathBuf> = vec![];
 
     let files = match read_dir(path) {
@@ -55,7 +31,7 @@ pub fn get_dir_items<'a>(path: &str) -> Vec<PathBuf> {
 pub fn get_apps_from_files(files: Vec<PathBuf>) -> Vec<String> {
     let infer = get_custom_infer();
 
-    // TODO: make this a map
+    // TODO: make this a map?
     let mut new_files: Vec<String> = vec![];
 
     for file in files {
--- src-tauri/src/services/mod.rs
+++ src-tauri/src/services/mod.rs
@@ -1,1 +1,2 @@
 pub mod file_handler;
+mod mime_type_handler;
